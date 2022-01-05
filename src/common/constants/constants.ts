55d2ab428b7a3835a78fe0d032a4e98672e0f695
DgtalBug <dgtalbug@Air.local>
2022-01-05 12:56:30 +0530

    strategy pattern with init

---
+++ src/common/constants/constants.ts
@@ -0,0 +1,6 @@
+export const DesignPatterns = {
+  strategy: {
+    title: 'STRATEGY PATTERN',
+    message: 'The Strategy pattern defines a family of algorithms, encapsulates each one, and makes them interchangeable. Strategy lets the algorithm vary independently from the clients that use it.'
+  }
+}
